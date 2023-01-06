9b59bc6073ec70f32871818faf3f626ce104e6e7
Justin Xing <justinxing3@gmail.com>
2023-01-06 17:57:36 -0500

    Added terminal functionality (commands and webpage redirection)

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Declarations
+let search = document.querySelector('.search');
+let inputText = search.value;
+
 // When the user scrolls the page, execute stickyHeader
 window.onscroll = function() {stickyHeader()};
 
@@ -15,3 +19,44 @@ function stickyHeader() {
     header.classList.remove("sticky");
   }
 }
+
+// Functions to be called upon input text + enter press
+let output = document.querySelector('.output');
+
+function ls () {
+  output.textContent = 'Try typing cd with one of these (ex. "cd about"): about resume projects interests myCat';
+  search.value = '';
+}
+
+function cd (s) {
+  if (s === 'about') {
+    window.location.href = "about.html";
+  } else if (s === 'resume') {
+    window.location.href = "resume.html";
+  } else if (s === 'projects') {
+    window.location.href = "projects.html";
+  } else if (s === 'myCat') {
+    window.location.href = "myCat.html";
+  }
+  search.value = '';
+}
+
+// Executing functions upon text input + enter press
+
+function readTextInput () {
+  if (inputText.substring(0, 2) === 'cd') {
+    cd(inputText.substring(3));
+  } else if (inputText.substring(0, 2) === 'ls') {
+    ls();
+  } else {
+    output.textContent = 'Enter a valid input! Try ls!'
+  }
+}
+
+search.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    inputText = search.value;
+    readTextInput();
+  }
+})
